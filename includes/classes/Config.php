cd09b29182a59c4bf435607bd5d85c88b5e5d15d
Luishernandez2017 <luis.hernandez2017@gmail.com>
2017-07-05 17:40:44 +0200

    Update Config.php

--- includes/classes/Config.php
+++ includes/classes/Config.php
@@ -3,7 +3,7 @@ ob_start();//Turns on output buffering
 session_start();//starts session
 
 $timezone = date_default_timezone_set("Europe/Amsterdam");
-$con = mysqli_connect("localhost", "Alexander","Great", "social");
+$con = mysqli_connect("localhost", "root","", "social");
 
 if(mysqli_connect_errno()){
     echo "Error:". mysqli_connect_errno();
